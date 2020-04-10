132e2a8edee44daab0fecbd85f3ba5799bde07ec
winterbreezesrule <48916147+winterbreezesrule@users.noreply.github.com>
2020-04-10 19:34:29 -0400

    Getting started on stuff
    I'm doing turnip crime in Animal Crossing, so this gives me something to do while I AFK.

--- DLException.java
+++ DLException.java
@@ -1,9 +1,82 @@
-public class DLException {
+import java.sql.*;
+import java.util.*;
+import java.io.*;
 
+/**
+  *
+  * A class to handle all exceptions and log them for the various classes
+  * built into the project files. 
+  *
+  * @author Jay Long
+  * ISTE-330 Section 01
+  * @since 2020-04-10
+  *
+  */
+public class DLException extends Exception {
+
+    Exception e = new Exception();
+    ArrayList<ArrayList<String>> info = new ArrayList<ArrayList<String>>();
+    
     /**
       *
-      * METHOD TO WRITE TO LOG FILE GOES HERE
+      * Constructor that accepts an exception.
+      *
+      * @param _e is the exception passed in.
       *
       */
+    public DLException(Exception _e) {
+        e = _e;
+        writeLog();
+    } // end constructor that accepts exception
+    
+    /**
+      *
+      * Constructor that accepts an exception and a 2D ArrayList containing
+      * additional info.
+      *
+      * @param _e is the exception passed in.
+      * @param _info is the 2D ArrayList containing additional information.
+      *
+      */
+    public DLException(Exception _e, ArrayList<ArrayList<String>> _info) {
+        e = _e;
+        info = _info;
+        writeLog();
+    } // end constructor with exception and string data
+    
+    /**
+      *
+      * Writes information to a log file that already exists. Will always write information
+      * about the exception and the date, and will add any additional info if it exists.
+      *
+      */
+    private void writeLog() {
+        try {
+            // create a printwriter to write to the log file
+            FileWriter fw = new FileWriter("errors.log", true);
+            PrintWriter pw = new PrintWriter(fw);
+            // get the current time
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            // write information about the exception to the log file
+            pw.println("Exception info: ");
+            e.printStackTrace(pw);
+            // check to see if any additional info was given, and if it was,
+            // write it to the log file
+            if (info != null) {
+                for (ArrayList<String> data : info) {
+                    pw.println(data.toString());
+                } // end for loop
+            } // end if loop
+            // write the current time to the log file
+            pw.println("Current time: " + timestamp.toString());
+            pw.println();
+            // close the printwriter
+            pw.close();
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Log file does not exist.");
+        } catch (IOException ioe) {
+            System.out.println("Error could not be logged.");
+        } // end try-catch
+    } // end writeLog()
 
 } // end DLException
--- MySQLDatabase.java
+++ MySQLDatabase.java
@@ -1,10 +1,73 @@
+import java.sql.*;
+import java.util.*;
+
+/**
+  *
+  * A class to connect to a MySQL database. 
+  * Written for the class project.
+  *
+  * @author Jay Long
+  * ISTE-330 Section 01
+  * @since 2020-04-10
+  *
+  */
 public class MySQLDatabase {
 
+    private String database;
+    private String user;
+    private String pass;
+    private Connection conn;
+    private ArrayList<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
+    
     /**
       *
-      * CONNECT AND CLOSE METHODS GO HERE
+      * The constructor for this class. Because someone using it may have
+      * different credentials on their MySQL instance installed on their machine,
+      * the only valid constructor takes a username and password. Because the
+      * database remains the same throughout, that does not need to be added.
       *
       */
+
+    public MySQLDatabase(String _user, String _pass) {
+        user = _user;
+        pass = _pass;
+        database = "jdbc:mysql://localhost/travel?useSSL=false";
+    } // end constructor
+    
+    /**
+      *
+      * Method to connect to the MySQL database.
+      *
+      * @return true if connection was successful. An exception is thrown otherwise.
+      * 
+      */
+    public boolean connect() throws DLException {
+        try {
+            conn = DriverManager.getConnection(database, user, pass);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Connection could not be created.");
+            throw new DLException(e);
+        } // end try-catch
+    } // end connect()
+    
+    /**
+      *
+      *
+      * Method to close the connection to the MySQL Database.
+      *
+      * @return true if the connection was closed successfully. An
+      * exception is thrown otherwise.
+      */
+    public boolean close() throws DLException {
+        try {
+            conn.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Connection could not be closed.");
+            throw new DLException(e);
+        } // end try-catch
+    } // end close()
       
     /**
       *
