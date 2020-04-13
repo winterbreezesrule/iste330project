cff38eab58bd9fa0b3bf5c580c0bcbc93582896a
Hansel Leal <hl4442@rit.edu>
2020-04-13 16:14:40 -0400

    Merge branch 'master' of F:\RIT Classes\ISTE330 - Database Connectivity\Group Project\gitSource\iste330project with conflicts.

--- Submission/src/MainTest.java
+++ Submission/src/MainTest.java
@@ -1,7 +1,7 @@
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        MySQLDatabase test = new MySQLDatabase("", "");
+        MySQLDatabase test = new MySQLDatabase("student", "student");
         try {
             test.connect();
             System.out.println("connected.");
